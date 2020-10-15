4b44356e65cee3eff0d979da8fc465c350bdfdb4
Matheus Mitestainer <matheusmitestainer@outlook.com>
2020-10-14 23:33:09 -0300

    NLW#3 - Day 3

---
+++ src/utils/mapIconOptions.ts
@@ -0,0 +1,10 @@
+import Leaflet from 'leaflet'
+
+import mapMarkerImg from '../images/map-marker.svg'
+
+export default Leaflet.icon({ 
+    iconUrl: mapMarkerImg,
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
